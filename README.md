bc3af93ee893f2e4d3af0b4e4d6d0317f462fac4
Tim McHugh <tim.mchugh@me.com>
2026-03-14 16:33:21 +0000

    updated to have calibration built in

--- README.md
+++ README.md
@@ -1,10 +1,246 @@
 # van-tilt-sensor-custom-card
+
+Custom Home Assistant Lovelace card showing van tilt.
+
+Install resource:
+
+- URL: /local/van-tilt-card/van-tilt-card.js
+- Type: JavaScript Module
+
+The card supports a built-in visual editor with only these fields:
+
+- Side to Side Sensor
+- Front to Back Sensor
+- Side to Side Zero
+- Front to Back Zero
+- Invert Side to Side
+- Invert Front to Back
+- Multiplier
+
+Equivalent YAML:
+
+```yaml
+type: custom:van-tilt-card
+entity_side_to_side: sensor.filtered_x_angle
+entity_front_to_back: sensor.filtered_y_angle
+side_to_side_zero: 0
+front_to_back_zero: 0
+invert_side_to_side: false
+invert_front_to_back: false
+multiplier: 1
+decimals: 1
+```
+
+Full example with your existing relay/preset buttons:
+
+```yaml
+type: custom:van-tilt-card
+entity_side_to_side: sensor.filtered_x_angle
+entity_front_to_back: sensor.filtered_y_angle
+side_to_side_zero: 1.0
+front_to_back_zero: 2.0
+invert_side_to_side: false
+invert_front_to_back: false
+multiplier: 1
+decimals: 1
+buttons:
+  - name: Rear Left Up
+    entity: switch.power_board_relay_4
+  - name: Rear Left Down
+    entity: switch.power_board_relay_3
+  - name: Rear Right Up
+    entity: switch.power_board_relay_2
+  - name: Rear Right Down
+    entity: switch.power_board_relay_5
+  - name: Driving Preset
+    entity: input_boolean.drive_preset
+  - name: Auto Level
+    entity: input_boolean.level_preset
+```
+
+Legacy aliases still supported: entity_x, entity_y, x_offset, y_offset, invert_x, invert_y.# van-tilt-sensor-custom-card
+
+Custom Home Assistant Lovelace card showing van tilt.
+
+Install resource:
+
+- URL: /local/van-tilt-card/van-tilt-card.js
+- Type: JavaScript Module
+
+Minimal config:
+
+```yaml
+type: custom:van-tilt-card
+entity_side_to_side: sensor.filtered_x_angle
+entity_front_to_back: sensor.filtered_y_angle
+```
+
+Manual calibration config:
+
+```yaml
+side_to_side_zero: 0        # Set this to zero side-to-side when parked level
+front_to_back_zero: 0       # Set this to zero front-to-back when parked level
+invert_side_to_side: false  # Flip side-to-side direction if needed
+invert_front_to_back: false # Flip front-to-back direction if needed
+multiplier: 1               # Shared scale factor for both axes
+decimals: 1                 # Display precision
+```
+
+Full example:
+
+```yaml
+type: custom:van-tilt-card
+entity_side_to_side: sensor.filtered_x_angle
+entity_front_to_back: sensor.filtered_y_angle
+side_to_side_zero: 1.0
+front_to_back_zero: 2.0
+invert_side_to_side: false
+invert_front_to_back: false
+multiplier: 1
+decimals: 1
+buttons:
+  - name: Rear Left Up
+    entity: switch.power_board_relay_4
+  - name: Rear Left Down
+    entity: switch.power_board_relay_3
+  - name: Rear Right Up
+    entity: switch.power_board_relay_2
+  - name: Rear Right Down
+    entity: switch.power_board_relay_5
+  - name: Driving Preset
+    entity: input_boolean.drive_preset
+  - name: Auto Level
+    entity: input_boolean.level_preset
+```
+
+Legacy aliases still supported: entity_x, entity_y, x_offset, y_offset, invert_x, invert_y.# van-tilt-sensor-custom-card
+
+Custom Home Assistant Lovelace card showing van tilt.
+
+Install resource:
+
+- URL: /local/van-tilt-card/van-tilt-card.js
+- Type: JavaScript Module
+
+Minimal config:
+
+```yaml
+type: custom:van-tilt-card
+entity_side_to_side: sensor.filtered_x_angle
+entity_front_to_back: sensor.filtered_y_angle
+```
+
+Manual calibration config:
+
+```yaml
+side_to_side_zero: 0        # Set this to zero side-to-side when parked level
+front_to_back_zero: 0       # Set this to zero front-to-back when parked level
+invert_side_to_side: false  # Flip side-to-side direction if needed
+invert_front_to_back: false # Flip front-to-back direction if needed
+decimals: 1                 # Display precision
+```
+
+Full example:
+
+```yaml
+type: custom:van-tilt-card
+entity_side_to_side: sensor.filtered_x_angle
+entity_front_to_back: sensor.filtered_y_angle
+side_to_side_zero: 1.0
+front_to_back_zero: 2.0
+invert_side_to_side: false
+invert_front_to_back: false
+decimals: 1
+buttons:
+  - name: Rear Left Up
+    entity: switch.power_board_relay_4
+  - name: Rear Left Down
+    entity: switch.power_board_relay_3
+  - name: Rear Right Up
+    entity: switch.power_board_relay_2
+  - name: Rear Right Down
+    entity: switch.power_board_relay_5
+  - name: Driving Preset
+    entity: input_boolean.drive_preset
+  - name: Auto Level
+    entity: input_boolean.level_preset
+```
+
+Legacy aliases still supported: entity_x, entity_y, x_offset, y_offset, invert_x, invert_y.# van-tilt-sensor-custom-card
+
 Custom home assistant lovelace card showing how tilted the van is
 
 To install the card, create a new folder in your "homeassistant/www/" directory called "van-tilt-card" and download the files to this directory. Then in Home Assistant go to Configuration - Lovelace Dashboards - Resources and click "Add Resource". Enter "/local/van-tilt-card/van-tilt-card.js" for the URL and "JavaScript Module" for the type and click create. Back in your main dashboard, you can now manually add the new card with the following code:
 
-type: 'custom:van-tilt-card'<br>
-entity_x: sensor.filtered_x_angle<br>
-entity_y: sensor.filtered_y_angle<br>
+```yaml
+type: custom:van-tilt-card
+entity_side_to_side: sensor.filtered_x_angle
+entity_front_to_back: sensor.filtered_y_angle
+```
+
+Simple options:
+
+```yaml
+side_to_side_zero: 0        # Manual zero for side-to-side axis
+front_to_back_zero: 0       # Manual zero for front-to-back axis
+invert_side_to_side: false  # Flip side-to-side direction
+invert_front_to_back: false # Flip front-to-back direction
+decimals: 1              # Display decimal places
+```
+
+In-card settings UI:
+
+- Use `- / +` to change zero values.
+- Use `Invert` to flip each axis direction.
+- Use `Set Current As Zero` while parked level.
+
+Cross-device persistence (recommended):
+
+Create helpers in Home Assistant UI and point the card to them.
+
+```yaml
+entity_side_to_side_zero: input_number.van_side_to_side_zero
+entity_front_to_back_zero: input_number.van_front_to_back_zero
+entity_invert_side_to_side: input_boolean.van_invert_side_to_side
+entity_invert_front_to_back: input_boolean.van_invert_front_to_back
+show_settings_controls: true
+zero_adjust_step: 0.1
+```
+
+If helper entities are not provided, settings UI still works for the current dashboard session.
+
+Example:
+
+```yaml
+type: custom:van-tilt-card
+entity_side_to_side: sensor.filtered_x_angle
+entity_front_to_back: sensor.filtered_y_angle
+side_to_side_zero: 1.0
+front_to_back_zero: 2.0
+invert_side_to_side: false
+invert_front_to_back: false
+entity_side_to_side_zero: input_number.van_side_to_side_zero
+entity_front_to_back_zero: input_number.van_front_to_back_zero
+entity_invert_side_to_side: input_boolean.van_invert_side_to_side
+entity_invert_front_to_back: input_boolean.van_invert_front_to_back
+show_settings_controls: true
+zero_adjust_step: 0.1
+decimals: 1
+buttons:
+  - name: Rear Left Up
+    entity: switch.power_board_relay_4
+  - name: Rear Left Down
+    entity: switch.power_board_relay_3
+  - name: Rear Right Up
+    entity: switch.power_board_relay_2
+  - name: Rear Right Down
+    entity: switch.power_board_relay_5
+  - name: Driving Preset
+    entity: input_boolean.drive_preset
+  - name: Auto Level
+    entity: input_boolean.level_preset
+```
+
+Legacy keys are still supported for backward compatibility: entity_x, entity_y, x_offset, y_offset, invert_x, invert_y.
 
 ![alt text](https://github.com/CF209/vanomation_website/blob/main/assets/img/tilt/tilt5.png)
--- van-tilt-card.js
+++ van-tilt-card.js
@@ -1,4 +1,22 @@
 class VanTiltCard extends HTMLElement {
+  static getConfigElement() {
+    return document.createElement('van-tilt-card-editor');
+  }
+
+  static getStubConfig() {
+    return {
+      type: 'custom:van-tilt-card',
+      entity_side_to_side: '',
+      entity_front_to_back: '',
+      side_to_side_zero: 0,
+      front_to_back_zero: 0,
+      invert_side_to_side: false,
+      invert_front_to_back: false,
+      multiplier: 1,
+      decimals: 1,
+    };
+  }
+
   constructor() {
     super();
     this.attachShadow({ mode: 'open' });
@@ -6,10 +24,24 @@ class VanTiltCard extends HTMLElement {
   }
 
   setConfig(config) {
-    if (!config.entity_x || !config.entity_y) {
-      throw new Error('You need to define entity_x and entity_y');
+    const sideToSideEntity = config.entity_side_to_side || config.entity_x;
+    const frontToBackEntity = config.entity_front_to_back || config.entity_y;
+
+    if (!sideToSideEntity || !frontToBackEntity) {
+      throw new Error('Both side-to-side and front-to-back entities are required');
     }
-    this.config = config;
+
+    this.config = {
+      entity_side_to_side: sideToSideEntity,
+      entity_front_to_back: frontToBackEntity,
+      side_to_side_zero: (config.side_to_side_zero ?? config.side_to_side_offset ?? config.x_offset ?? 0),
+      front_to_back_zero: (config.front_to_back_zero ?? config.front_to_back_offset ?? config.y_offset ?? 0),
+      multiplier: (config.multiplier ?? 1),
+      invert_side_to_side: (config.invert_side_to_side ?? config.invert_x ?? false),
+      invert_front_to_back: (config.invert_front_to_back ?? config.invert_y ?? false),
+      decimals: 1,
+      ...config,
+    };
   }
 
   getCardSize() {
@@ -69,39 +101,49 @@ class VanTiltCard extends HTMLElement {
       <div style="display: flex;">
         <div style="flex: 60%; text-align: center; position: relative;">
           <img class="van-side" src="/local/van-tilt-card/img/van_side.png" style="max-width: 100%; height: 100px;">
-          <p class="y-angle" style="position: absolute; top: 10%; left: 68%; transform: translate(-50%, -50%); color: black; font-size: 25px">0°</p>
+          <p class="front-to-back-angle" style="position: absolute; top: 10%; left: 68%; transform: translate(-50%, -50%); color: black; font-size: 25px">0°</p>
         </div>
         <div style="flex: 40%; text-align: center; position: relative;">
           <img class="van-back" src="/local/van-tilt-card/img/van_back.png" style="max-width: 100%; height: 100px;">
-          <p class="x-angle" style="position: absolute; top: 10%; left: 60%; transform: translate(-50%, -50%); color: black; font-size: 25px">0°</p>
+          <p class="side-to-side-angle" style="position: absolute; top: 10%; left: 60%; transform: translate(-50%, -50%); color: black; font-size: 25px">0°</p>
         </div>
       </div>
     `;
 
     this.shadowRoot.appendChild(card);
 
-    // Cache references
     this.presetContainer = card.querySelector('.preset-buttons');
     this.positionalContainer = card.querySelector('.positional-buttons');
     this.vanSideImg = card.querySelector('.van-side');
     this.vanBackImg = card.querySelector('.van-back');
-    this.angleXText = card.querySelector('.x-angle');
-    this.angleYText = card.querySelector('.y-angle');
+    this.frontToBackAngleText = card.querySelector('.front-to-back-angle');
+    this.sideToSideAngleText = card.querySelector('.side-to-side-angle');
 
     this.presetButtons = {};
     this.positionedButtons = {};
   }
 
   updateCard(hass) {
-    const entityX = this.config.entity_x;
-    const entityY = this.config.entity_y;
-    const xAngle = parseInt(hass.states[entityX]?.state || '0');
-    const yAngle = parseInt(hass.states[entityY]?.state || '0');
+    const sideToSideEntity = this.config.entity_side_to_side || this.config.entity_x;
+    const frontToBackEntity = this.config.entity_front_to_back || this.config.entity_y;
+    const rawSideToSide = parseFloat(hass.states[sideToSideEntity]?.state || '0');
+    const rawFrontToBack = parseFloat(hass.states[frontToBackEntity]?.state || '0');
 
-    this.angleXText.textContent = `${xAngle}°`;
-    this.angleYText.textContent = `${yAngle}°`;
-    this.vanSideImg.style.transform = `rotate(${yAngle * 2}deg)`;
-    this.vanBackImg.style.transform = `rotate(${xAngle * 2}deg)`;
+    const sideToSideSign = this.config.invert_side_to_side ? -1 : 1;
+    const frontToBackSign = this.config.invert_front_to_back ? -1 : 1;
+    const multiplier = Number(this.config.multiplier) || 1;
+    const sideToSideZero = Number(this.config.side_to_side_zero) || 0;
+    const frontToBackZero = Number(this.config.front_to_back_zero) || 0;
+    const rotationScale = 2;
+    const decimals = Math.max(0, Number(this.config.decimals) || 0);
+
+    const sideToSideAngle = ((Number.isFinite(rawSideToSide) ? rawSideToSide : 0) * sideToSideSign) + sideToSideZero;
+    const frontToBackAngle = ((Number.isFinite(rawFrontToBack) ? rawFrontToBack : 0) * frontToBackSign) + frontToBackZero;
+
+    this.sideToSideAngleText.textContent = `${sideToSideAngle.toFixed(decimals)}°`;
+    this.frontToBackAngleText.textContent = `${frontToBackAngle.toFixed(decimals)}°`;
+    this.vanSideImg.style.transform = `rotate(${frontToBackAngle * rotationScale * multiplier}deg)`;
+    this.vanBackImg.style.transform = `rotate(${sideToSideAngle * rotationScale * multiplier}deg)`;
 
     const buttonConfigs = this.config.buttons || [];
     const overrideMap = {
@@ -113,10 +155,9 @@ class VanTiltCard extends HTMLElement {
 
     const presetNames = ['Driving Preset', 'Auto Level'];
 
-    // Preset buttons
     if (Object.keys(this.presetButtons).length === 0) {
       this.presetContainer.innerHTML = '';
-      buttonConfigs.filter(btn => presetNames.includes(btn.name)).forEach(btn => {
+      buttonConfigs.filter((btn) => presetNames.includes(btn.name)).forEach((btn) => {
         const button = document.createElement('button');
         button.classList.add('preset-button');
         button.textContent = btn.name;
@@ -127,11 +168,8 @@ class VanTiltCard extends HTMLElement {
           const other = target === 'input_boolean.level_preset'
             ? 'input_boolean.drive_preset'
             : 'input_boolean.level_preset';
-        
-          // Turn off the other one
+
           hass.callService('input_boolean', 'turn_off', { entity_id: other });
-        
-          // Toggle the clicked one
           hass.callService('input_boolean', 'toggle', { entity_id: target });
         });
 
@@ -145,7 +183,6 @@ class VanTiltCard extends HTMLElement {
       button.classList.toggle('active', isOn);
     }
 
-    // Positional buttons
     if (Object.keys(this.positionedButtons).length === 0) {
       this.positionalContainer.innerHTML = '';
       buttonConfigs.forEach((btn, i) => {
@@ -163,7 +200,6 @@ class VanTiltCard extends HTMLElement {
           const domain = btn.entity.split('.')[0];
           hass.callService(domain, 'toggle', { entity_id: btn.entity });
 
-          // Exclusive toggle
           const pairMap = { 0: 1, 1: 0, 2: 3, 3: 2 };
           if (pairMap[i] !== undefined) {
             const otherBtn = buttonConfigs[pairMap[i]];
@@ -187,3 +223,155 @@ class VanTiltCard extends HTMLElement {
 }
 
 customElements.define('van-tilt-card', VanTiltCard);
+
+class VanTiltCardEditor extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+    this._config = {};
+    this._hass = null;
+  }
+
+  setConfig(config) {
+    this._config = {
+      type: 'custom:van-tilt-card',
+      entity_side_to_side: '',
+      entity_front_to_back: '',
+      side_to_side_zero: 0,
+      front_to_back_zero: 0,
+      invert_side_to_side: false,
+      invert_front_to_back: false,
+      multiplier: 1,
+      decimals: 1,
+      ...config,
+    };
+    this.render();
+  }
+
+  set hass(hass) {
+    this._hass = hass;
+    this.render();
+  }
+
+  get sensorEntities() {
+    if (!this._hass) {
+      return [];
+    }
+
+    return Object.keys(this._hass.states)
+      .filter((entityId) => entityId.startsWith('sensor.'))
+      .sort((left, right) => left.localeCompare(right));
+  }
+
+  updateConfig(key, value) {
+    const newConfig = {
+      ...this._config,
+      [key]: value,
+    };
+
+    this._config = newConfig;
+    this.dispatchEvent(new CustomEvent('config-changed', {
+      detail: { config: newConfig },
+      bubbles: true,
+      composed: true,
+    }));
+  }
+
+  renderEntityOptions(selectedValue) {
+    const options = ['<option value="">Select sensor</option>'];
+    this.sensorEntities.forEach((entityId) => {
+      const selected = entityId === selectedValue ? ' selected' : '';
+      options.push(`<option value="${entityId}"${selected}>${entityId}</option>`);
+    });
+    return options.join('');
+  }
+
+  render() {
+    if (!this.shadowRoot) {
+      return;
+    }
+
+    const config = this._config || {};
+    this.shadowRoot.innerHTML = `
+      <style>
+        .editor {
+          display: grid;
+          gap: 12px;
+          padding: 16px 0;
+        }
+        .field {
+          display: grid;
+          gap: 6px;
+        }
+        label {
+          font-size: 14px;
+          font-weight: 600;
+        }
+        select,
+        input[type="number"] {
+          padding: 8px;
+          font: inherit;
+        }
+        .toggle {
+          display: flex;
+          align-items: center;
+          gap: 8px;
+        }
+      </style>
+      <div class="editor">
+        <div class="field">
+          <label for="entity-side-to-side">Side to Side Sensor</label>
+          <select id="entity-side-to-side">${this.renderEntityOptions(config.entity_side_to_side || '')}</select>
+        </div>
+        <div class="field">
+          <label for="entity-front-to-back">Front to Back Sensor</label>
+          <select id="entity-front-to-back">${this.renderEntityOptions(config.entity_front_to_back || '')}</select>
+        </div>
+        <div class="field">
+          <label for="side-to-side-zero">Side to Side Zero</label>
+          <input id="side-to-side-zero" type="number" step="0.1" value="${Number(config.side_to_side_zero ?? 0)}">
+        </div>
+        <div class="field">
+          <label for="front-to-back-zero">Front to Back Zero</label>
+          <input id="front-to-back-zero" type="number" step="0.1" value="${Number(config.front_to_back_zero ?? 0)}">
+        </div>
+        <div class="field">
+          <label for="multiplier">Visual Multiplier</label>
+          <input id="multiplier" type="number" step="0.1" value="${Number(config.multiplier ?? 1)}">
+        </div>
+        <label class="toggle">
+          <input id="invert-side-to-side" type="checkbox"${config.invert_side_to_side ? ' checked' : ''}>
+          <span>Invert Side to Side</span>
+        </label>
+        <label class="toggle">
+          <input id="invert-front-to-back" type="checkbox"${config.invert_front_to_back ? ' checked' : ''}>
+          <span>Invert Front to Back</span>
+        </label>
+      </div>
+    `;
+
+    this.shadowRoot.getElementById('entity-side-to-side')?.addEventListener('change', (event) => {
+      this.updateConfig('entity_side_to_side', event.target.value);
+    });
+    this.shadowRoot.getElementById('entity-front-to-back')?.addEventListener('change', (event) => {
+      this.updateConfig('entity_front_to_back', event.target.value);
+    });
+    this.shadowRoot.getElementById('side-to-side-zero')?.addEventListener('change', (event) => {
+      this.updateConfig('side_to_side_zero', Number(event.target.value) || 0);
+    });
+    this.shadowRoot.getElementById('front-to-back-zero')?.addEventListener('change', (event) => {
+      this.updateConfig('front_to_back_zero', Number(event.target.value) || 0);
+    });
+    this.shadowRoot.getElementById('multiplier')?.addEventListener('change', (event) => {
+      this.updateConfig('multiplier', Number(event.target.value) || 1);
+    });
+    this.shadowRoot.getElementById('invert-side-to-side')?.addEventListener('change', (event) => {
+      this.updateConfig('invert_side_to_side', event.target.checked);
+    });
+    this.shadowRoot.getElementById('invert-front-to-back')?.addEventListener('change', (event) => {
+      this.updateConfig('invert_front_to_back', event.target.checked);
+    });
+  }
+}
+
+customElements.define('van-tilt-card-editor', VanTiltCardEditor);
